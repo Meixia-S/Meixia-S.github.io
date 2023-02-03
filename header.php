06f135bb1743cf958f2beae3fa2e406da0ea9b99
Meixia-S <108845505+Meixia-S@users.noreply.github.com>
2023-02-03 10:41:07 -0500

    Update header.php

--- header.php
+++ header.php
@@ -4,35 +4,35 @@
     
     <style>
       .navcontainer {
-    position: rel;
-    width: auto;
-    margin: 0;
-}
+         position: rel;
+         width: auto;
+         margin: 0;
+      }
+      
+      .topbar {
+        background-color:rgb(31, 38, 48);
+        overflow: hidden;
+      }
 
-.topbar {
-    background-color:rgb(31, 38, 48);
-    overflow: hidden;
-}
+      .topbar a {
+        float: left;
+        color:rgb(141, 156, 179);
+        text-align: center;
+        padding: 14px 16px;
+        font-size: 27px;
+      }
 
-.topbar a {
-    float: left;
-    color:rgb(141, 156, 179);
-    text-align: center;
-    padding: 14px 16px;
-    font-size: 27px;
-}
+      .topbar i {
+        float: left;
+        color: rgb(141, 156, 179);
+        text-align: center;
+        padding: -1px 12px;
+        font-size: 30px;
+      }
 
-.topbar i {
-    float: left;
-    color: rgb(141, 156, 179);
-    text-align: center;
-    padding: -1px 12px;
-    font-size: 30px;
-}
-
-.topbar a:hover {
-    background-color: rgb(119, 167, 236);
-}
+      .topbar a:hover {
+         background-color: rgb(119, 167, 236);
+      }
     </style>
   </header>
    
@@ -45,9 +45,10 @@
                 <a href="references.html" id="references">References</a>
                 <a href="https://www.instagram.com/meixia_sindelar/" id="instagram" title="follow me @Meixia_Sindelar"><i class="fa fa-instagram"></i></a>
                 <a href="https://www.linkedin.com/in/meixia-sindelar/" id="linkedin" title="check out my LinkedIn profile"><i class="fa fa-linkedin-square"></i></a>
+                <a href="https://github.com/Meixia-S" id="github" title="check out my GitHub profile"><i class="fa fa-github"></i></a>
                 <a href="mailto:meixia2001@gmail.com" id="email" title="click to send me an email"><i class="fa fa-envelope"></i></a>
                 <a href="tel:*6304600067" id="number" title="click to contact me"><i class="fa fa-phone"></i></a>
             </div>  
       </div>
-    </body>
-    </html>
+   </body>
+   </html>
